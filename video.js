6e947343654aa348651ee1720d4150ad4135712c
tech849 <smarakd64@gmail.com>
2024-10-13 21:45:31 +0530

    video.js

--- video.js
+++ video.js
@@ -1,3 +1,3 @@
 function videos(){
-document.getElementById("usresult").innerHTML="<iframe src='/home-videos.html' width='100%' height='100%'></iframe>";
+document.getElementById("usresult").innerHTML="<iframe src='/SDL/home-videos.html' width='100%' height='100%'></iframe>";
 }
