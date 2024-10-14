5e66894dd9df46e24119f33dbe38254a67e6b91d
tech849 <smarakd64@gmail.com>
2024-10-14 20:20:22 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 function home(){
-document.getElementById("usresult").innerHTML="hello";
+document.getElementById("usresult").innerHTML="<embed src='/SDL/home.html'width='100%' height='100%'></embed>";
 }
-document.getElementById("usresult").innerHTML="hello";
+document.getElementById("usresult").innerHTML="<embed src='/SDL/home.html'width='100%' height='100%'></embed>";
