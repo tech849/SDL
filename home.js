1b477f4ec963a0d90648324bee14bb94d7d5e8d0
tech849 <smarakd64@gmail.com>
2024-10-14 15:45:09 +0530

    home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-document.getElementById("usresult").innerHTML="<embed src='/SDL/home.html' width='100%'height='100%'></embed><br><p id='usresult1'></p><br><p id='usresult2'></p><br><p id='usresult3'></p>";
+document.getElementById("usresult").innerHTML="<embed src='/SDL/home.html' width='100%'height='100%'></embed><br><p id='usresult1'></p>";
 function home(){
 document.getElementById("usresult").innerHTML="<embed src='/SDL/home.html' width='100%'height='100%'></embed>";
 }
