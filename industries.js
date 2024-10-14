f7924c13bb81db248bd0fe3d73713275a8b47c7a
tech849 <smarakd64@gmail.com>
2024-10-14 12:58:48 +0530

    industries.js

--- industries.js
+++ industries.js
@@ -1,4 +1,3 @@
-document.getElementById("indus").innerHTML="<embed src='/SDL/industries.html' width='100%' height='100%'></embed>";
 function industries(){
 document.getElementById("usresult").innerHTML="<embed src='/SDL/industries.html' width='100%' height='100%'></embed>";
 }
