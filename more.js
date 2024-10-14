7d5fac4c12e2f59d013bceab49acedcf70c734b1
tech849 <smarakd64@gmail.com>
2024-10-14 12:16:57 +0530

    more.js

--- more.js
+++ more.js
@@ -1,3 +1,6 @@
 function cate(){
 document.getElementById("usresult").innerHTML="<embed src='/SDL/more.html' width='100%' height='100%'></embed>";
 }
+function industries(){
+document.getElementById("usresult").innerHTML="<embed src='/SDL/industries.html' width='100%' height='100%'></embed>";
+}
