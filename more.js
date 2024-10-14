c1cf888c27052c3101a7792356db402b8cf141f0
tech849 <smarakd64@gmail.com>
2024-10-14 12:27:13 +0530

    more.js

--- more.js
+++ more.js
@@ -1,6 +1,3 @@
 function cate(){
 document.getElementById("usresult").innerHTML="<embed src='/SDL/more.html' width='100%' height='100%'></embed>";
 }
-function industries(){
-document.getElementById("usresult").innerHTML="<embed src='/SDL/industries.html' width='100%' height='100%'></embed>";
-}
