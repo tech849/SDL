48bb5cc459d79c2a01d314682e891086bee8e1d3
tech849 <smarakd64@gmail.com>
2024-10-15 19:59:42 +0530

    Update industry.js

--- industry.js
+++ industry.js
@@ -1,13 +1,2 @@
-function industries(){
-document.getElementById("usresult").innerHTML="<html><head><link rel='stylesheet' href='/SDL/scroll.css'></head><body><h3>AUTOMOBILE INDUSTRIES</h3><br><div class='scrll-imgs'><embed src='/SDL/tata-motors.jpg' width='200' height='200'></embed><embed src='/SDL/mahindra-motors.jpg' width='200' height='200'></embed><embed src='/SDL/maruti-suzuki.jpg' width='200' height='200'></embed><embed src='/SDL/rolls-royce.jpg' width='200' height='200'></embed><embed src='/SDL/ford-motors.jpg' width='200' height='200'></embed><embed src='/SDL/ferrari.jpg' width='200' height='200'></embed><embed src='/SDL/bently.jpg' width='200' height='200'></embed><embed src='/SDL/bhugati.jpg' width='200' height='200'></embed><embed src='/SDL/lamborghini.jpg' width='200' height='200'></embed></div></body></html>";
-
-document.getElementById("usresult").innerHTML="<html><head><link rel='stylesheet' href='/SDL/scroll.css'></head><body><br><h3>BEVRAGE INDUSTRIES</h3><br><div class='scrll-imgs'><embed src='/SDL/coca-cola.jpg' width='200' height='200'></embed><embed src='/SDL/pepsi.jpg' width='200' height='200'></embed><embed src='/SDL/parle-agro.jpg' width='200' height='200'></embed><embed src='/SDL/bisleri.jpg' width='200' height='200'></embed>  <embed src='/SDL/nestle.jpg' width='200' height='200'></embed><embed src='/SDL/dabur.jpg' width='200' height='200'></embed><embed src='/SDL/britania.jpg' width='200' height='200'></embed><embed src='/SDL/hindustan-unilever.jpg' width='200' height='200'></embed></div>";
-
-
-
-document.getElementById("usresult").innerHTML="<html><head><link rel='stylesheet' href='/SDL/scroll.css'></head><body><br><h3>FOOD FACTORIES</h3><br><div class='scrll-imgs'><embed src='/SDL/amul-icecream.jpg' width='200' height='200'></embed><embed src='/SDL/amul-butter.jpg' width='200' height='200'></embed><embed src='/SDL/maggie.jpg' width='200' height='200'></embed><embed src='/SDL/lays.jpg' width='200' height='200'></embed></div>";
-
-
-
-document.getElementById("usresult").innerHTML="<html><head><link rel='stylesheet' href='/SDL/scroll.css'></head><body><br><h3>TEXTILE INDUSTRIES</h3><br><div class='scrll-imgs'><embed src='/SDL/tata-motors.jpg' width='200' height='200'></embed><embed src='/SDL/mahindra-motors.jpg' width='200' height='200'></embed><embed src='/SDL/maruti-suzuki.jpg' width='200' height='200'></embed><embed src='/SDL/rolls-royce.jpg' width='200' height='200'></embed></div></body></html>";
-}
+<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"><iframe src="https://drive.google.com/file/d/1ZOdOusuGW3iuWv8XhP9jEgmBM2PQmGo5/preview" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen scrolling="no" allow="encrypted-media;"></iframe></div>
+  <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"><iframe src="https://drive.google.com/file/d/1F_ssGemeaIAW3-J6oJUPgufg0VzmqZkt/preview" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen scrolling="no" allow="encrypted-media;"></iframe></div>
