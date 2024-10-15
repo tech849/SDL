d9afc4d7b037fa9bf694162b10be4d1eccba1a58
tech849 <smarakd64@gmail.com>
2024-10-15 21:17:56 +0530

    Update quiz-games.js

--- quiz-games.js
+++ quiz-games.js
@@ -1,3 +1,3 @@
 function brain(){
-document.getElementById("usresult").innerHTML="<html><head></head><body><table><tr><td><embed src='/SDL/java.jpg'width='200'height=''200'></embed></td><td><embed src='/SDL/c.jpg'width='200'height=''200'></embed></td></tr><tr><td><embed src='/SDL/python.jpg'width='200'height=''200'></embed></td><td><embed src='/SDL/html.jpg'width='200'height=''200'></embed></td></tr></body></html>";
+document.getElementById("usresult").innerHTML="<html><head></head><body><table><tr><td><embed src='/SDL/java.jpg'width='200'height=''200'onclick='java_quiz();'></embed></td><td><embed src='/SDL/c.jpg'width='200'height=''200'></embed></td></tr><tr><td><embed src='/SDL/python.jpg'width='200'height=''200'></embed></td><td><embed src='/SDL/html.jpg'width='200'height=''200'></embed></td></tr></table></body></html>";
 }
