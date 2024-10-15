4e550debfeadc9817f9a03d21103136b7f6bac8d
tech849 <smarakd64@gmail.com>
2024-10-15 19:43:19 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 function home(){
-document.getElementById("usresult").innerHTML="<html><body> <p id='scientist'></p><p id='usresult11'></p><p id='usresult10'></p></body></html>";
+document.getElementById("usresult").innerHTML="<html><body> <p id='scientist'></p><p id='usresult11'></p><p id='usresult10'></p><script src='/SDL/great-scientist.js'></script><script src='/SDL/industries.js'></script></body></html>";
 }
-document.getElementById("usresult").innerHTML="<html><body> <p id='scientist'></p><p id='usresult11'></p><p id='usresult10'></p></body></html>";
+document.getElementById("usresult").innerHTML="<html><body> <p id='scientist'></p><p id='usresult11'></p><p id='usresult10'></p><script src='/SDL/great-scientist.js'></script><script src='/SDL/industries.js'></script></body></html>";
