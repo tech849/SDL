3ab491072ab666e912a01da47fe618086d2d6bf5
tech849 <smarakd64@gmail.com>
2024-10-15 21:24:38 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -1,3 +1,7 @@
+function documentary(){
+document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick=' the_master_of_lightning();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick=' dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
+}
+  //main
 document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick=' the_master_of_lightning();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick=' dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
 function the_master_of_lightning(){
   window.open("/SDL/the-master-of-lightning.html");
