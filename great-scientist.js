47fd3c4d06099c60aea3fac2da39fd509cc2aaf8
tech849 <smarakd64@gmail.com>
2024-10-15 17:11:40 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -1,1 +1,1 @@
-
+document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
