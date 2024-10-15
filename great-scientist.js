ea05e4e1ed717b1903dd07bb1c65a8e3d5620fdc
tech849 <smarakd64@gmail.com>
2024-10-15 20:45:32 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -1,1 +1,7 @@
-document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST</h3><br><div class='scrll-imgs'><a href='/SDL/the-master-of-lightning.html'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'></embed></a><a href='/SDL/dc-vs-ac.html'><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div></a>";
+document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick=' the_master_of_lightning();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick=' dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
+function the_master_of_lightning(){
+  window.open("/SDL/the-master-of-lightning.html");
+}
+function dc_vs_ac(){
+  window.open("/SDL/dc-vs-ac.html");
+}
