11773b9b55deb000234f86c5ffe154a362f48005
tech849 <smarakd64@gmail.com>
2024-10-15 16:04:56 +0530

    Update quiz-games.js

--- quiz-games.js
+++ quiz-games.js
@@ -1,2 +1,3 @@
-
+function brain(){
 document.getElementById("usresult").innerHTML="<html><head></head><body><table><tr><td>JAVA QUIZZ</td><td>C QUIZZ</td></tr><tr><td>PYTHON QUIZZ</td><td>HTML QUIZZ</td></tr></body></html>";
+}
