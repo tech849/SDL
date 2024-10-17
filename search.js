c204b14e0ff9a33ce93c821d9b1f35c4d320ed5d
tech849 <smarakd64@gmail.com>
2024-10-17 10:29:41 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,3 @@
 function srep(){
-document.getElementById("searchinp").innerHTML="<p id='menu'></p><input style='width:200px;' type='search' onkeypress='search();'id='usquerry'placeholder='search..'><button onclick='menu();'style='font-size:40px;'>X</button>";
+document.getElementById("searchinp").innerHTML="<p id='menu'></p><input style='width:200px;' type='search' onkeypress='search();'id='usquerry'placeholder='search..'><button onclick='menu();'style='font-size:20px;'>X</button>";
 }
