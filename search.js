6a6b4350ea1fd6175cc2a32e6f32f6f9cceb9396
tech849 <smarakd64@gmail.com>
2024-10-17 12:14:32 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,5 +1,7 @@
-
-const effects = setTimeout(srep,5000);
+function srep(){
+document.getElementById("searchinp1").innerHTML="<p id='searchinp1'><embed src='/SDL/search.gif'width='200px'height='50dp'></embed></p>";
+}
+const effects = setTimeout(srep,9000);
 function srep(){
 document.getElementById("searchinp1").innerHTML="<p id='menu'></p><input style='width:200px;' type='search' onkeypress='search();'id='usquerry'placeholder='search..'><button onclick='menu();'style='font-size:20px;'>X</button>";
 }
