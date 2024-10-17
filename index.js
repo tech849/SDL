a4584bcf270932d43246ba885bc092d2dd50956f
tech849 <smarakd64@gmail.com>
2024-10-17 10:27:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 function search(){
 var a = document.getElementById("usquerry").value;
-if(a.toLowerCase() == "konark"){
+if(a.toLowerCase() == "konark temple"){
 document.getElementById("usresult").innerHTML="<a href='/SDL/konark.html' style='underline:none;'><table border='1px solid black'style='background-color:#8FBC8B;'><tr><th>NAME</th><th>LOCATION</th><th>STATE</th></tr><tr><td>KONARK SUN TEMPLE  TEMPLE</td><td>puri,chandrabhaga</td><td>ODISHA</td></tr></table></a>";
 }
+if(a.toLowerCase() == "jagannath  temple"){
+document.getElementById("usresult").innerHTML="<a href='/SDL/jagannath.html'><table border='1px solid black'><tr><th>NAME</th><th>LOCATION</th><th>STATE</th><tr><td>JAGANNATH TEMPLE</td><td>bada danda,puri</td><td>ODISHA</td></tr></table></a>";
+}
+if(a.toLowerCase() == "lingaraj temple"){
+document.getElementById("usresult").innerHTML="<a href='/SDL/lingaraj.html'><table border='1px solid black'><tr><th>NAME</th><th>LOCATION</th><th>STATE</th><tr><td>LINGARAJ TEMPLE</td><td>bhubneswar</td><td>ODISHA</td></tr></table></a>";
+}
+if(a.toLowerCase() == "ratan tata"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/ratan-naval-tata.jpg' width='200' height='200'></embed></td><td>RATAN NAVAL TATA<br><i>oral story</i></td></tr></table>";
+}
+  if(a.toLowerCase() == "industries"){
+document.getElementById("usresult").innerHTML="<embed src='/SDL/industries.html'width='100%'height='100%'></embed>";
+  }
+  if(a.toLowerCase() == "quiz games"){
+document.getElementById("usresult").innerHTML="<embed src='/SDL/brain.html'width='100%'height='100%'></embed>";
+  }
+  if(a.toLowerCase() == "java quiz"){
+document.getElementById("usresult").innerHTML="<embed src='/SDL/java-quiz.html'width='100%'height='100%'></embed>";
+  }
+  if(a.toLowerCase() == "nikola tesla"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/the-master-of-lightning.jpg' width='200'height='200'></embed></td><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+   if(a.toLowerCase() == "dc vs ac"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+  if(a.toLowerCase() == "thomas edison"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+  if(a.toLowerCase() == "the master of lightning"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/the-master-of-lightning.jpg' width='200'height='200'></embed></td><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+
+  //  single word search resulth
+
 if(a.toLowerCase() == "jagannath"){
 document.getElementById("usresult").innerHTML="<a href='/SDL/jagannath.html'><table border='1px solid black'><tr><th>NAME</th><th>LOCATION</th><th>STATE</th><tr><td>JAGANNATH TEMPLE</td><td>bada danda,puri</td><td>ODISHA</td></tr></table></a>";
 }
@@ -12,19 +45,31 @@ document.getElementById("usresult").innerHTML="<a href='/SDL/lingaraj.html'><tab
 if(a.toLowerCase() == "tata"){
 document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/ratan-naval-tata.jpg' width='200' height='200'></embed></td><td>RATAN NAVAL TATA<br><i>oral story</i></td></tr></table>";
 }
-  if(a.toLowerCase() == "industries"){
-document.getElementById("usresult").innerHTML="<embed src='/SDL/industries.html'width='100%'height='100%'></embed>";
-  }
   if(a.toLowerCase() == "quiz"){
 document.getElementById("usresult").innerHTML="<embed src='/SDL/brain.html'width='100%'height='100%'></embed>";
   }
-  if(a.toLowerCase() == "quiz games"){
-document.getElementById("usresult").innerHTML="<embed src='/SDL/brain.html'width='100%'height='100%'></embed>";
-  }
-  if(a.toLowerCase() == "java quiz"){
-document.getElementById("usresult").innerHTML="<embed src='/SDL/java-quiz.html'width='100%'height='100%'></embed>";
-  }
   if(a.toLowerCase() == "java"){
 document.getElementById("usresult").innerHTML="<p>JAVA</p><br><p>JAVA QUIZ</p>";
+  }
+   if(a.toLowerCase() == "tesla"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/the-master-of-lightning.jpg' width='200'height='200'></embed></td><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+  if(a.toLowerCase() == "master"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/the-master-of-lightning.jpg' width='200'height='200'></embed></td><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+  if(a.toLowerCase() == "lightning"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/the-master-of-lightning.jpg' width='200'height='200'></embed></td><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+   if(a.toLowerCase() == "dc"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+  if(a.toLowerCase() == "ac"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+  if(a.toLowerCase() == "thomas"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
+  }
+  if(a.toLowerCase() == "edison"){
+document.getElementById("usresult").innerHTML="<table><tr><td><embed src='/SDL/dc-vs-ac.jpg' width='200'height='200'></embed></td></tr></table> ";
   }
 }
