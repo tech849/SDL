0072f07877df4596f81066b9f45801e0e8e80b57
tech849 <smarakd64@gmail.com>
2024-10-17 12:08:54 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-document.getElementById("menu").innerHTML="<table border='none' outline='none'style='position:fixed;top:0px;cursor:pointer;width:100%;height:50dp;'><td onclick='home();'onchange='menu()'>&#8962;HOME</td><td onclick='videos();'>VIDEOS</td><td><p id='searchinp1'></p></td><td onclick='cate();'>&#9776;</td></table>";
+document.getElementById("menu").innerHTML="<table border='none' outline='none'style='position:fixed;top:0px;cursor:pointer;width:100%;height:50dp;'><td onclick='home();'onchange='menu()'>&#8962;HOME</td><td onclick='videos();'>VIDEOS</td><td><p id='searchinp1'><embed src='/SDL/search.gif'width='200px'height='50dp'></embed></p></td><td onclick='cate();'>&#9776;</td></table>";
 function menu(){
   document.getElementById("menu").innerHTML="<table border='none' outline='none'style='position:fixed;top:0px;cursor:pointer;width:100%;'><td onclick='home();'onchange='menu()'>&#8962;HOME</td><td onclick='videos();'>VIDEOS</td><td><p id='searchinp'><button onclick='srep();'>SEARCH</button></p></td><td onclick='cate();'>&#9776;</td></table>";
 
