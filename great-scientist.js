4e55c688e35eed980e1ffba5dc402dbc5c95b576
tech849 <smarakd64@gmail.com>
2024-10-17 17:24:22 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -1,13 +1,9 @@
 function documentary(){
-document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><p id='rp'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='load_the_master_of_lightning();rdirect();'></embed></p><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
+document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='the_master_of_lightning();rdirect();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
 }
   //main
-document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><p id='rp'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='load_the_master_of_lightning();rdirect();'></embed></p><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
-function rdirect(){
-  document.getElementById("rp").innerHTML="<p height='200' width='200' bgcolor='white'style='text-align:center;color:red;'>Redirecting...</p>";
-}
-const t_m_o_f = setTimeout("load_the_master_of_lightning,4999");
-function load_the_master_of_lightning(){
+document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='the_master_of_lightning();rdirect();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
+function the_master_of_lightning(){
   window.open("/SDL/the-master-of-lightning.html");
 }
 function dc_vs_ac(){
