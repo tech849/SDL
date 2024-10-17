51d8904963ce66d8962a7f46dacbaafed5e10921
tech849 <smarakd64@gmail.com>
2024-10-17 14:19:52 +0530

    Update search.js

--- search.js
+++ search.js
@@ -3,5 +3,5 @@ document.getElementById("searchinp").innerHTML="<p id='searchinp1'>Loading...</p
 }
 const effects = setTimeout(srep,3000);
 function srep(){
-document.getElementById("searchinp1").innerHTML="<p id='menu'></p><input style='width:200px;' type='search' onkeypress='search();'id='usquerry'placeholder='search..'><button onclick='menu();'style='font-size:20px;'>X</button>";
+document.getElementById("searchinp1").innerHTML="<p id='menu'></p><input style='width:200px;' type='search' onkeypress='search();'id='usquerry'placeholder='search..'>";
 }
