1c48f6488effd8c6c6488024cc3af61d0a0e3e65
tech849 <smarakd64@gmail.com>
2024-10-19 21:13:17 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -4,7 +4,7 @@ document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES
   //main
 document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='load_lightning()'id='the_master_of_lightning'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
 function load_lightning(){
-  document.getElementById("the_master_of_lightning").innerHTML="<embed src='/SDL/redirect.gif'width='200'height='200'></embed>";
+  document.getElementById("the_master_of_lightning").innerHTML="<p border='2px solid yellow'bgcolor='white' width='200'height=200'font-size='30px'></p>";
   const loadtesla = setTimeout(the_master_of_lightning,5000);
 }
 function the_master_of_lightning(){
