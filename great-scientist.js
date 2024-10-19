5a51c08f99f1380117efa07924dc67d6236552fe
tech849 <smarakd64@gmail.com>
2024-10-19 20:38:29 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -2,7 +2,7 @@ function documentary(){
 document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='the_master_of_lightning();rdirect();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
 }
   //main
-document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><p id='the_master_of_lightning'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='load_lightning()'></embed></p><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
+document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='load_lightning()'id='the_master_of_lightning'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
 function load_lightning(){
   document.getElementById("the_master_of_lightning").innerHTML="<embed src='/SDL/sample.html'width='200'height='200'></embed>";
   const loadtesla = setTimeout(the_master_of_lightning,5000);
