5dc01851716c307461fdcc461a1d6eb6bd927aa8
tech849 <smarakd64@gmail.com>
2024-10-20 18:48:49 +0530

    Update hist-temples.js

--- hist-temples.js
+++ hist-temples.js
@@ -2,7 +2,7 @@ document.getElementById("usresult11").innerHTML="<h3>TEMPLES</h3><br><div id='lo
 //redirect main
 
 function ldkonark(){
-document.getElementById("loadmain").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>Redirecting...To<br>(KONARK TEMPLE)</p>";
+document.getElementById("ldmain").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>Redirecting...To<br>(KONARK TEMPLE)</p>";
 const loadingkonark = setTimeout(loadmain, 5000);
 const resetkonark = setTimeout(rst1, 6000);
                }
