a73eeb9519dbabde330569306a601906dfd51fe0
tech849 <smarakd64@gmail.com>
2024-10-20 18:22:02 +0530

    Update hist-temples.js

--- hist-temples.js
+++ hist-temples.js
@@ -1,1 +1,20 @@
-document.getElementById("usresult11").innerHTML="<h3>TEMPLES</h3><br><div class='scrll-imgs'><embed src='/SDL/konark.jpg' width='200' height='200'></embed><embed src='/SDL/jagannath.jpg' width='200' height='200'></embed><embed src='/SDL/lingaraj.jpg' width='200' height='200'></embed></div><br><h3>NATION</h3><br><div class='scrll-imgs'><embed src='/SDL/indian.jpg' width='200' height='200'></embed><embed src='/SDL/pakistan.jpg' width='200' height='200'></embed><embed src='/SDL/bangladesh.jpg' width='200' height='200'></embed><embed src='/SDL/myanmar.jpg' width='200' height='200'></embed><embed src='/SDL/china.jpg' width='200' height='200'></embed></div>";
+document.getElementById("usresult11").innerHTML="<h3>TEMPLES</h3><br><div class='scrll-imgs'><embed src='/SDL/konark.jpg' width='200' height='200'onclick='ldkonark();'></embed><embed src='/SDL/jagannath.jpg' width='200' height='200'></embed><embed src='/SDL/lingaraj.jpg' width='200' height='200'></embed></div><br><h3>NATION</h3><br><div class='scrll-imgs'><embed src='/SDL/indian.jpg' width='200' height='200'></embed><embed src='/SDL/pakistan.jpg' width='200' height='200'></embed><embed src='/SDL/bangladesh.jpg' width='200' height='200'></embed><embed src='/SDL/myanmar.jpg' width='200' height='200'></embed><embed src='/SDL/china.jpg' width='200' height='200'></embed></div>";
+//redirect main
+
+function ldkonark(){
+             window.open("/SDL/the-master-of-lightning.html");
+                }
+//dv vs ac
+function ld3(){
+document.getElementById("lm").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>Redirecting...To<br>(KONARK TEMPLE)</p>";
+//const loading2 = setTimeout(load1, 5000);
+const reset2 = setTimeout(rst, 6000);
+               }
+  
+         function ldmain(){
+              // window.open("/SDL/konark.html");
+                }
+//refresh common
+function rst1(){
+location.reload();
+}
