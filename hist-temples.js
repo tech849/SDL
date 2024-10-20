1fdb384dc6a8cd1e7452fd8380969cb9bb1b0b8e
tech849 <smarakd64@gmail.com>
2024-10-20 18:23:32 +0530

    Update hist-temples.js

--- hist-temples.js
+++ hist-temples.js
@@ -1,4 +1,4 @@
-document.getElementById("usresult11").innerHTML="<h3>TEMPLES</h3><br><div class='scrll-imgs'><embed src='/SDL/konark.jpg' width='200' height='200'onclick='ldkonark();'></embed><embed src='/SDL/jagannath.jpg' width='200' height='200'></embed><embed src='/SDL/lingaraj.jpg' width='200' height='200'></embed></div><br><h3>NATION</h3><br><div class='scrll-imgs'><embed src='/SDL/indian.jpg' width='200' height='200'></embed><embed src='/SDL/pakistan.jpg' width='200' height='200'></embed><embed src='/SDL/bangladesh.jpg' width='200' height='200'></embed><embed src='/SDL/myanmar.jpg' width='200' height='200'></embed><embed src='/SDL/china.jpg' width='200' height='200'></embed></div>";
+document.getElementById("usresult11").innerHTML="<h3>TEMPLES</h3><br><div id='loadmain' class='scrll-imgs'><embed src='/SDL/konark.jpg' width='200' height='200'onclick='ldkonark();'></embed><embed src='/SDL/jagannath.jpg' width='200' height='200'></embed><embed src='/SDL/lingaraj.jpg' width='200' height='200'></embed></div><br><h3>NATION</h3><br><div class='scrll-imgs'><embed src='/SDL/indian.jpg' width='200' height='200'></embed><embed src='/SDL/pakistan.jpg' width='200' height='200'></embed><embed src='/SDL/bangladesh.jpg' width='200' height='200'></embed><embed src='/SDL/myanmar.jpg' width='200' height='200'></embed><embed src='/SDL/china.jpg' width='200' height='200'></embed></div>";
 //redirect main
 
 function ldkonark(){
@@ -6,7 +6,7 @@ function ldkonark(){
                 }
 //dv vs ac
 function ld3(){
-document.getElementById("lm").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>Redirecting...To<br>(KONARK TEMPLE)</p>";
+document.getElementById("loadmain").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>Redirecting...To<br>(KONARK TEMPLE)</p>";
 //const loading2 = setTimeout(load1, 5000);
 const reset2 = setTimeout(rst, 6000);
                }
