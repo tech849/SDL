81aac51c8a26dd4c8f7dcbaae0afbfb7c161fadf
tech849 <smarakd64@gmail.com>
2024-10-20 14:39:50 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -1,8 +1,8 @@
 function documentary(){
-document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs' id='lm'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='ld();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
+document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div class='scrll-imgs' id='lm'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='ld();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='ld2();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
 }
   //main
-document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div  id='lm' class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='ld();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='dc_vs_ac();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
+document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div  id='lm' class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='ld();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='ld2();'></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
 //function load_lightning(){
  // document.getElementById("usresult").innerHTML="<p border='2px solid yellow'bgcolor='white' width='200'height=200'font-size='30px'>redirecting</p>";
  // const loadtesla = setTimeout(the_master_of_lightning,5000);
@@ -25,6 +25,17 @@ const reset = setTimeout(rst, 2000);
          function load(){
              window.open("/SDL/the-master-of-lightning.html");
                 }
+//dv vs ac
+function ld2(){
+document.getElementById("lm").innerHTML="redirecting";
+const loading1 = setTimeout(load1, 1000);
+const reset1 = setTimeout(rst, 2000);
+               }
+  
+         function load(){
+             window.open("/SDL/dc-vs-ac.html");
+                }
+//refresh common
 function rst(){
 location.reload();
 }
