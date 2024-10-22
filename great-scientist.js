c51eefd7d8b455392205039ec4e18ea630edef79
tech849 <smarakd64@gmail.com>
2024-10-22 15:30:43 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -32,17 +32,17 @@ function ld3(){
 document.getElementById("lm").innerHTML="<table style='border-collapse:collapse;'><tr><td><embed src='/SDL/the-forgotten-genius.jpg'width='200'height='200'></embed></td><td><p id='l1'style='color:yellow;border-radius:20px;'></p></td><td><p id='l2'style='color:blue;background-color:transparent;border-radius:20px;'><embed src='/SDL/loading-circle.gif'width='16'height='16'style='border-radius:50%;border:2px solid yellow;'></embed></p></td></tr></table>";
   document.getElementById("l1").innerHTML="loading..";
                // const percentage =  setTimeout(fetched,5000);
- const p1 = setTimeout(process,5000);
-  const f1 = setTimeout(fetching,10000);
-  const r1 = setTimeout(redirecting,15000);
-  const av1 = setTimeout(avail,20000);
+ const p1 = setTimeout(process,2000);
+  const f1 = setTimeout(fetching,3000);
+  const r1 = setTimeout(redirecting,4000);
+  const av1 = setTimeout(avail,5000);
 //const loading2 = setTimeout(load1, 5000);
-const reset3 = setTimeout(rst, 21000);
+const reset3 = setTimeout(rst, 6000);
   //load per fetch
-  const perc1 = setTimeout(per1,5000);
-  const perc2 = setTimeout(per2,10000);
-  const perc3= setTimeout(per3,15000);
-  const perc4 = setTimeout(per4,18000);
+  const perc1 = setTimeout(per1,2000);
+  const perc2 = setTimeout(per2,3000);
+  const perc3= setTimeout(per3,4000);
+  const perc4 = setTimeout(per4,5000);
                }
   
          function load3(){
