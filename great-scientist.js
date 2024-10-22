d1d7324aaf2085fd15c11d4cdc45ea6d721aa116
tech849 <smarakd64@gmail.com>
2024-10-22 10:42:15 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -3,23 +3,12 @@ document.getElementById("usresult").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES
 }
   //main
 document.getElementById("scientist").innerHTML="<h3>GREAT SCIENTIST DOCUMENTARIES</h3><br><div  id='lm' class='scrll-imgs'><embed src='/SDL/the-master-of-lightning.jpg' width='200' height='200'onclick='ld();'></embed><embed src='/SDL/dc-vs-ac.jpg' width='200' height='200' onclick='ld2();'></embed><embed src='/SDL/the-forgotten-genius.jpg' width='200' height='200'onclick='ld3();'></embed></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></embed><embed src='/SDL/.jpg' width='200' height='200'></embed></div>";
-//function load_lightning(){
- // document.getElementById("usresult").innerHTML="<p border='2px solid yellow'bgcolor='white' width='200'height=200'font-size='30px'>redirecting</p>";
- // const loadtesla = setTimeout(the_master_of_lightning,5000);
-//}
-//function the_master_of_lightning(){
-//  window.open("/SDL/the-master-of-lightning.html");
-//}
-function dc_vs_ac(){
-  window.open("/SDL/dc-vs-ac.html");
-}
+
 //refresh
 function ld(){
 document.getElementById("lm").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>Redirecting ...To<br> (THE MASTER OF LIGHTNING)</p>";
 const loading = setTimeout(load, 5000);
 const reset = setTimeout(rst, 6000);
-
-
                }
   
          function load(){
@@ -27,37 +16,25 @@ const reset = setTimeout(rst, 6000);
                 }
 //dv vs ac
 function ld2(){
-document.getElementById("lm").innerHTML="<table><tr><td><p id='l1'></p></td><td><p id='l2'></p></td><td></tr></table><p id='t1'></p>";
-  document.getElementById("l1").innerHTML="loading..";
-                document.getElementById("t1").innerHTML="100%"; 
-                const loadfetch = setInterval(load,100);
-                const loadfetch1 = setTimeout(load2,4000);
-             const loadfetch2 = setTimeout(load1,4100);
-               }
-            
-         function load(){
-             document.getElementById("l2").innerHTML+=".";
-             
-         }
-         function load1(){
-             location.reload();
-         }
-         function load2(){
-             window.open("/SDL/dc-vs-ac.html");
-         }
-
-// the forgotten genius
-function load(){
-             window.open("/SDL/the-master-of-lightning.html");
-                }
-//dv vs ac
-function ld3(){
-document.getElementById("lm").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>UPLOADING SOON..</p>";
-//const loading2 = setTimeout(load1, 5000);
+document.getElementById("lm").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>Redirecting ...To<br> (DC VS AC)</p>";
+const loading2 = setTimeout(load2, 5000);
 const reset2 = setTimeout(rst, 6000);
                }
   
          function load2(){
+             window.open("/SDL/dc-vs-ac.html");
+                }
+
+// the forgotten genius
+
+//dv vs ac
+function ld3(){
+document.getElementById("lm").innerHTML="<p style='color:yellow;background-color:black;border:2px solid white;font-size:5vw;'>UPLOADING SOON..</p>";
+//const loading2 = setTimeout(load1, 5000);
+const reset3 = setTimeout(rst, 6000);
+               }
+  
+         function load3(){
               // window.open("/SDL/dc-vs-ac.html");
                 }
 //refresh common
