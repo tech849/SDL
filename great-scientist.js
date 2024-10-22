ffbf7a41313a0d24c9dc85b7c9adebeddf391f9b
tech849 <smarakd64@gmail.com>
2024-10-22 19:45:30 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -78,13 +78,13 @@ function avail(){
          }
 // loader percentage
 function per1(){
-             document.getElementById("l2").innerHTML="25%";            
+             document.getElementById("l2").innerHTML="<b style='background-color:yellow;border:1px solid red;'>25%</b>";            
          }
 function per2(){
-             document.getElementById("l2").innerHTML="50%";            
+             document.getElementById("l2").innerHTML="<b style='background-color:yellow;border:1px solid red;'>50%</b>";            
          }
 function per3(){
-             document.getElementById("l2").innerHTML="75%";            
+             document.getElementById("l2").innerHTML="<b style='background-color:yellow;border:1px solid red;'>75%</b>";            
          }
 
 function per4(){
