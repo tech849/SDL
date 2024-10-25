d14c018b3b730ecd359f1b6237ec83ce44664b7e
tech849 <smarakd64@gmail.com>
2024-10-25 20:27:41 +0530

    Update great-scientist.js

--- great-scientist.js
+++ great-scientist.js
@@ -23,16 +23,16 @@ const reset = setTimeout(rst, 6000);
                 }
 //dv vs ac
 function ld2(){
-document.getElementById("lm").innerHTML="<table style='border-collapse:collapse;background-image: linear-gradient(to top, #dbdcd7 0%, #dddcd7 24%, #e2c9cc 30%, #e7627d 46%, #b8235a 59%, #801357 71%, #3d1635 84%, #1c1a27 100%);border-radius:10px;'><tr><td><embed src='/SDL/the-forgotten-genius.jpg'width='150'height='150'></embed></td><td><h2 color='yellow'>THE MASTER OF LIGHTNING</h2><br><button onclick='watch1();'style='background:blur(100px);border:2px solid white;color:green;'>WATCH</button><button onclick='download1();'style='background:blur(100px);border:2px solid white;color:green;'>DOWNLOAD</button><button onclick='read1();'style='background:blur(100px);border:2px solid white;color:green;'>READ</button><br><p id='nikola2'></p></td></tr></table>";
+document.getElementById("lm").innerHTML="<table style='border-collapse:collapse;background-image: linear-gradient(to top, #dbdcd7 0%, #dddcd7 24%, #e2c9cc 30%, #e7627d 46%, #b8235a 59%, #801357 71%, #3d1635 84%, #1c1a27 100%);border-radius:10px;'><tr><td><embed src='/SDL/dc-vs-ac.jpg'width='150'height='150'></embed></td><td><h2 color='yellow'>DC VS AC</h2><br><button onclick='watch1();'style='background:blur(100px);border:2px solid white;color:green;'>WATCH</button><button onclick='download1();'style='background:blur(100px);border:2px solid white;color:green;'>DOWNLOAD</button><button onclick='read1();'style='background:blur(100px);border:2px solid white;color:green;'>READ</button><br><p id='nikola2'></p></td></tr></table>";
  
                }
   
          
-function watch(){
-   document.getElementById("nikola1").innerHTML="<table><tr><td><p id='l1'style='color:yellow;border-radius:20px;background-image: linear-gradient(to right, #434343 0%, black 100%);'>Loading...</p></td><td><p id='l2'style='color:blue;background-image: linear-gradient(to top, #0fd850 0%, #f9f047 100%);border-radius:20px;'><embed src='/SDL/loading-circle.gif'width='16'height='16'style='border-radius:50%;border:2px solid yellow;'></embed></p></td></tr></table>";      
+function watch2(){
+   document.getElementById("nikola2").innerHTML="<table><tr><td><p id='l1'style='color:yellow;border-radius:20px;background-image: linear-gradient(to right, #434343 0%, black 100%);'>Loading...</p></td><td><p id='l2'style='color:blue;background-image: linear-gradient(to top, #0fd850 0%, #f9f047 100%);border-radius:20px;'><embed src='/SDL/loading-circle.gif'width='16'height='16'style='border-radius:50%;border:2px solid yellow;'></embed></p></td></tr></table>";      
  const f12= setTimeout(fetching,1000);
   const r12 = setTimeout(redirecting,2000);
-  const av1 = setTimeout(displaying,4000);
+  const av12 = setTimeout(displaying,4000);
   
 //const loading2 = setTimeout(load1, 5000);
 //const reset3 = setTimeout(rst, 6000);
@@ -41,9 +41,9 @@ function watch(){
   const perc22= setTimeout(per2,2100);
   const perc32= setTimeout(per3,3100);
   const perc42= setTimeout(per4,4100);
-  const stream32= setTimeout(stream_tfg,6000);
+  const stream32= setTimeout(stream_tfg1,6000);
 }
-function download(){
+function download2(){
    document.getElementById("nikola1").innerHTML="<table><tr><td><p id='l1'style='color:yellow;border-radius:20px;background-image: linear-gradient(to right, #434343 0%, black 100%);'>Loading...</p></td><td><p id='l2'style='color:blue;background-image: linear-gradient(to top, #0fd850 0%, #f9f047 100%);border-radius:20px;'><embed src='/SDL/loading-circle.gif'width='16'height='16'style='border-radius:50%;border:2px solid yellow;'></embed></p></td></tr></table>";      
  const f_12 = setTimeout(fetching,1000);
   const r_12= setTimeout(redirecting,2000);
@@ -53,22 +53,22 @@ function download(){
   //load per fetch
   const perc_12= setTimeout(per1,1100);
   const perc_22= setTimeout(per2,2100);
-  const perc_3= setTimeout(per3,3100);
-  const perc_4 = setTimeout(per4,4100);
-  const download1 = setTimeout(dwn,6000);
+  const perc_32= setTimeout(per3,3100);
+  const perc_42= setTimeout(per4,4100);
+  const download12 = setTimeout(dwn1,6000);
 }
-function read(){
+function read2(){
    document.getElementById("nikola1").innerHTML="<table><tr><td><p id='l1'style='color:yellow;border-radius:20px;background-image: linear-gradient(to right, #434343 0%, black 100%);'>Loading...</p></td><td><p id='l2'style='color:blue;background-image: linear-gradient(to top, #0fd850 0%, #f9f047 100%);border-radius:20px;'><embed src='/SDL/loading-circle.gif'width='16'height='16'style='border-radius:50%;border:2px solid yellow;'></embed></p></td></tr></table>";      
- const f1_2 = setTimeout(fetching,1000);
-  const r1_2 = setTimeout(redirecting,2000);
-  const av1_2 = setTimeout(avail,4000);
+ const f1_22 = setTimeout(fetching,1000);
+  const r1_22= setTimeout(redirecting,2000);
+  const av1_22= setTimeout(avail,4000);
 //const loading2 = setTimeout(load1, 5000);
-const reset3_4 = setTimeout(rst, 6000);
+const reset3_42 = setTimeout(rst, 6000);
   //load per fetch
-  const perc1_2 = setTimeout(per1,1100);
-  const perc2_3 = setTimeout(per2,2100);
-  const perc3_4= setTimeout(per3,3100);
-  const perc4_5 = setTimeout(per4,4100);
+  const perc1_22 = setTimeout(per1,1100);
+  const perc2_32 = setTimeout(per2,2100);
+  const perc3_42= setTimeout(per3,3100);
+  const perc4_52 = setTimeout(per4,4100);
 }
 
 
