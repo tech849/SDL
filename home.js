6bf3293f9bfb81c2a6a08c851bc8f72b1625791e
tech849 <smarakd64@gmail.com>
2024-10-25 12:39:02 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 function home(){
-document.getElementById("usresult").innerHTML="<embed src='/SDL/home.html'width='100%'height='100%'></embed>";
+document.getElementById("usresult").innerHTML="<iframe src='/SDL/home.html'width='100%'height='100%'></iframe>";
 }
-document.getElementById("usresult").innerHTML="<embed src='/SDL/home.html'width='100%'height='100%'></embed>";
+document.getElementById("usresult").innerHTML="<iframe src='/SDL/home.html'width='100%'height='100%'></iframe>";
