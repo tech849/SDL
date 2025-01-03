0bc159a947bbab949844614a4756e7e1b2226ca7
tech849 <smarakd64@gmail.com>
2025-01-03 13:55:48 +0530

    Update level.js

--- level.js
+++ level.js
@@ -1,3 +1,3 @@
 function level1(){
-document.getElementById("jq1").innerHTML="<table><tr><td><a href='/SDL/jq1.html'>LEVEL-1</a></td>&nbsp;&nbsp;&nbsp;<a href='/SDL/jq2.html'>LEVEL-2</a></td><td>&nbsp;&nbsp;&nbsp;LEVEL-3</td></tr></table>";
+document.getElementById("jq1").innerHTML="<table><tr><td><a href='/SDL/jq1.html'>LEVEL-1</a></td><td>&nbsp;&nbsp;&nbsp;<a href='/SDL/jq2.html'>LEVEL-2</a></td><td>&nbsp;&nbsp;&nbsp;LEVEL-3</td></tr></table>";
 }
