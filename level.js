d9bc891048c83e2a15e70d0950640db0b94201a8
tech849 <smarakd64@gmail.com>
2025-01-03 12:10:43 +0530

    Create level.js

---
+++ level.js
@@ -0,0 +1,3 @@
+function level1(){
+document.getElementById("jq1").innerHTML="<table><tr><td><a href='/SDL/jq1.html'>LEVEL-1</a></td><a href='/SDL/jq2.html'>&nbsp;&nbsp;&nbsp;LEVEL-2</a></td><td>&nbsp;&nbsp;&nbsp;LEVEL-3</td></tr></table>";
+}
