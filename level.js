c7bc419cdb832ee855462b68a180c0e649212d59
tech849 <smarakd64@gmail.com>
2025-01-03 14:26:58 +0530

    Update level.js

--- level.js
+++ level.js
@@ -1,3 +1,3 @@
 function level1(){
-document.getElementById("jq1").innerHTML="<table><tr><td><a href='/SDL/jq1.html'><embed src='/SDL/level1.jpg' width='100'height='100'></embed></a></td><td>&nbsp;&nbsp;&nbsp;<a href='/SDL/jq2.html'><embed src='/SDL/level2.jpg' width='100' height='100'></embed></a></td><td>&nbsp;&nbsp;&nbsp;<embed src='/SDL/level3.jpg' width='100' height='100'></embed></td></tr></table>";
+document.getElementById("jq1").innerHTML="<table><tr><td><a href='/SDL/jq1.html'><embed src='/SDL/level1.jpg' width='100'height='100'></embed></a></td><td>&nbsp;&nbsp;&nbsp;<a href='/SDL/jq2.html'><embed src='/SDL/level2.jpg' width='100' height='100'></embed></a></td><td>&nbsp;&nbsp;&nbsp;<embed src='/SDL/level3.jpg' width='100' height='100'></embed></td><td>&nbsp;&nbsp;&nbsp;<embed src='/SDL/level4.jpg' width='100' height='100'></embed></td><td>&nbsp;&nbsp;&nbsp;<embed src='/SDL/level5.jpg' width='100' height='100'></embed></td></tr></table>";
 }
